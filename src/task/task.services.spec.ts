639ede205f74ba724824319cb809321cd87a220f
santoadji21 <ahmad.aji.santoso21@gmail.com>
2022-09-11 12:20:42 +0700

    feat: testing

---
+++ src/task/task.services.spec.ts
@@ -0,0 +1,64 @@
+import { TaskService } from './task.service';
+import { NotFoundException } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { TaskStatus } from './task-status.enum';
+import { TaskRepository } from './task.repository';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Ariel',
+  id: 'someId',
+  password: 'somePassword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let tasksService: TaskService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TaskService,
+        { provide: TaskRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TaskService);
+    tasksRepository = module.get(TaskRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls TasksRepository.findOne and returns the result', async () => {
+      const mockTask = {
+        title: 'Test title',
+        description: 'Test desc',
+        id: 'someId',
+        status: TaskStatus.OPEN,
+      };
+
+      tasksRepository.findOne.mockResolvedValue(mockTask);
+      const result = await tasksService.getTaskById('someId', mockUser);
+      expect(result).toEqual(mockTask);
+    });
+
+    it('calls TasksRepository.findOne and handles an error', async () => {
+      tasksRepository.findOne.mockResolvedValue(null);
+      expect(tasksService.getTaskById('someId', mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+});
